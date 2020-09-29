20144b422f51409cac568a0cf1fecce87dac079e
danielmponda <dlmponda@gmail.com>
2020-09-30 00:40:24 +0200

    Rearrange elements

--- src/compenent/Recipe.js
+++ src/compenent/Recipe.js
@@ -4,8 +4,12 @@ function Recipe({ props }) {
   // console.log(props.recipe)
   return (
     <div className="recipe">
+      <div>
+        {' '}
+        <h6>{props.recipe.label}</h6>
+      </div>
+
       <img className="img-label" src={props.recipe.image} alt="" />
-      <h6>{props.recipe.label}</h6>
     </div>
   )
 }
