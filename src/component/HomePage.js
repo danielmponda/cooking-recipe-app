750b350ff53c106ce2264aecc79666f8c31bb933
danielmponda <dlmponda@gmail.com>
2020-10-09 01:23:52 +0200

    Add Home Page component

---
+++ src/component/HomePage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Search from './Search'
+import Recipes from './Recipes'
+import Pagination from './Pagination'
+
+function Home({
+  currentPage,
+  recipesPerPage,
+  search,
+  setSearch,
+  recipes,
+  setCurrentPage,
+}) {
+  const indexOfLastRecipe = currentPage * recipesPerPage
+  const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage
+  const currentRecipe = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe)
+
+  // Change Page
+  function paginate(pageNumber) {
+    setCurrentPage(pageNumber)
+  }
+
+  return (
+    <div className="home-page">
+      <Search search={search} setSearch={setSearch} />
+      <Recipes reciper={currentRecipe} />
+      <Pagination
+        recipesPerPage={recipesPerPage}
+        totalRecipes={recipes.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+    </div>
+  )
+}
+
+export default Home
