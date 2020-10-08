9f40fdea07b0b6517f8860fda881842373aeaa12
danielmponda <dlmponda@gmail.com>
2020-10-09 01:25:41 +0200

    Add Link tag on each recipe

--- src/component/Recipes.js
+++ src/component/Recipes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Recipe from './Recipe'
+import { Link } from 'react-router-dom'
 
 function Recipes({ reciper }) {
   console.log(reciper)
   return (
-    <div className="recipe-wrapper">
+    <div className="recipe-wrapper" id="recipe-wrapper">
       {reciper.map((recipe, index) => (
-        <Recipe key={index} props={recipe} />
+        <Link key={index} to={`/recipeview/${recipe.recipe.label}`}>
+          <Recipe key={index} props={recipe} />
+        </Link>
       ))}
     </div>
   )
