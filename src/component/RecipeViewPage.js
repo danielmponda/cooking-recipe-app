3b29c8fc83a38638cc6bdee2515b2cd931d3fba1
danielmponda <dlmponda@gmail.com>
2020-10-25 17:21:03 +0200

    Fitler item results and return the 1st item

--- src/component/RecipeViewPage.js
+++ src/component/RecipeViewPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Recipes from './Recipes'
 
 function RecipeView({ id, recipes }) {
   useEffect(() => {
@@ -10,50 +11,64 @@ function RecipeView({ id, recipes }) {
 
   todosArray = recipes.filter((e) => e.recipe.label === id)
 
+  const related = recipes.filter((e) => e.recipe.label !== id)
+
+  console.log(todosArray)
+
+  const todosArrayFilter = todosArray.filter((reps, index) => {
+    console.log(index)
+    if (index == 0) {
+      return true
+    }
+  })
+  console.log(todosArrayFilter)
+  const view = 'ralated'
   return (
     <div className="recipeview-page">
-      <div className="recipeview-compenent">
-        {todosArray.map((element, index) => (
-          <div key={index} className="item-wrapper">
-            <div className="item-sec-a">
-              <div>
-                <img
-                  className="img-label-view"
-                  alt="pic"
-                  src={element.recipe.image}
-                />
-              </div>
+      <div className="recipeview-compenent"></div>
 
-              <div className="recipe-view">
-                <div className="recipe-info">
-                  <h4>{element.recipe.label}</h4>
-                  <p className="calories">
-                    Calories : {element.recipe.calories.toFixed(2)}
-                  </p>
-                  <p className="deit">
-                    {' '}
-                    {element.recipe.healthLabels.join(' | ')}
-                  </p>
-                </div>
-              </div>
+      {todosArrayFilter.map((element, index) => (
+        <div key={index} className="item-wrapper">
+          <div className="item-sec-a">
+            <div className="view-img">
+              <img
+                className="img-label-view"
+                alt="pic"
+                src={element.recipe.image}
+              />
             </div>
 
-            <div className="item-sec-b">
-              <div className="recipe-view-ingr">
-                <h5 className="ingredient-title">
+            <div className="recipe-view">
+              <div className="recipe-info">
+                <h4>{element.recipe.label}</h4>
+                <p className="calories">
+                  Calories : {element.recipe.calories.toFixed(2)}
+                </p>
+                <p className="deit">
                   {' '}
-                  {element.recipe.ingredientLines.length} Ingredient
-                </h5>
-                <ul>
-                  {element.recipe.ingredientLines.map((element, index) => (
-                    <li key={index}>{element}</li>
-                  ))}
-                </ul>
+                  {element.recipe.healthLabels.join(' | ')}
+                </p>
               </div>
             </div>
           </div>
-        ))}
-      </div>
+          <div className="item-sec-b">
+            <span className="ingredient-title">
+              {element.recipe.ingredientLines.length} Ingredient
+            </span>
+            <div className="recipe-view-ingr">
+              <ul>
+                {element.recipe.ingredientLines.map((element, index) => (
+                  <li key={index}>{element}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      {related ? <div className="relatedtitle">also try </div> : null}
+
+      <Recipes reciper={related} view={view} />
     </div>
   )
 }
