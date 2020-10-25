91603859a1f2713a1f7e98bbcc0cf00c26c53634
danielmponda <dlmponda@gmail.com>
2020-10-25 17:19:38 +0200

    Limit related items to less than 4

--- src/component/Recipes.js
+++ src/component/Recipes.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import Recipe from './Recipe'
+import Related from './RelatedRecipes'
 import { Link } from 'react-router-dom'
 
-function Recipes({ reciper }) {
-  console.log(reciper)
-  return (
-    <div className="recipe-wrapper" id="recipe-wrapper">
-      {reciper.map((recipe, index) => (
-        <Link key={index} to={`/recipeview/${recipe.recipe.label}`}>
-          <Recipe key={index} props={recipe} />
-        </Link>
-      ))}
-    </div>
-  )
+function Recipes({ reciper, view }) {
+  console.log(view)
+
+  const related = reciper.filter((reps, index) => {
+    if (index < 4) {
+      return true
+    }
+  })
+
+  console.log(related)
+
+  if (view === 'search') {
+    return (
+      <div className="recipe-wrapper" id="recipe-wrapper">
+        {reciper.map((recipe, index) => (
+          <Link key={index} to={`/recipeview/${recipe.recipe.label}`}>
+            <Recipe key={index} props={recipe} />
+          </Link>
+        ))}
+      </div>
+    )
+  } else {
+    return (
+      <div className="recipe-wrapper" id="recipe-wrapper">
+        {related.map((recipe, index) => (
+          <Link key={index} to={`/recipeview/${recipe.recipe.label}`}>
+            <Related key={index} props={recipe} />
+          </Link>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Recipes
