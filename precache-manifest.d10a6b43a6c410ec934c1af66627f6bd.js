2fb8dd93b3e8f3cb50c6924680fc8dc8d851cc0a
danielmponda <dlmponda@gmail.com>
2020-10-26 09:33:13 +0200

    Updates

---
+++ precache-manifest.d10a6b43a6c410ec934c1af66627f6bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a54643b63018e4944a1e89c875d915",
+    "url": "/cooking-recipe-app/index.html"
+  },
+  {
+    "revision": "10f07b63dc063f84979c",
+    "url": "/cooking-recipe-app/static/css/main.f5be8309.chunk.css"
+  },
+  {
+    "revision": "7dd60ed251cf94fb8676",
+    "url": "/cooking-recipe-app/static/js/2.e382f94a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cooking-recipe-app/static/js/2.e382f94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f07b63dc063f84979c",
+    "url": "/cooking-recipe-app/static/js/main.87ed9480.chunk.js"
+  },
+  {
+    "revision": "874aeeab844528cf4611",
+    "url": "/cooking-recipe-app/static/js/runtime-main.2dd692e5.js"
+  },
+  {
+    "revision": "92bac27ce4d0493c7a6368d86509b057",
+    "url": "/cooking-recipe-app/static/media/logo3.92bac27c.png"
+  }
+]);
